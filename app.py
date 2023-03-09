ca4734caab66d4e0f4923f416c22de6f2a12c6e5
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-03-09 15:22:29 -0800

    add working start page and q1

--- app.py
+++ app.py
@@ -7,3 +7,32 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+
+responses = []
+
+@app.get('/')
+
+def start_page ():
+    """Loads Survey Opening"""
+
+    title = survey.title
+    instructions = survey.instructions
+    print ("title ", title , "inst", instructions)
+    return render_template(
+        "survey_start.html", title = title, instructions = instructions)
+
+@app.post("/begin")
+
+def begin_survey ():
+    """Directs user to question 0 on start click"""
+    return redirect ("/questions/0")
+
+@app.get("/questions/<int:question_num>")
+
+def test (question_num):
+    question = survey.questions[question_num]
+    prompt = question.prompt
+    choices = question.choices
+    return render_template ("question.html", prompt = prompt, choices = choices)
+
