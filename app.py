ff1bba95f8307dfb4cf61734790935e2d32ceb7a
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-03-09 17:16:24 -0800

    change global to session cookie

--- app.py
+++ app.py
@@ -9,32 +9,28 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 
-responses = []
-
 @app.get('/')
 def start_page ():
     """Loads Survey Opening"""
-
     title = survey.title
     instructions = survey.instructions
     print ("title ", title , "inst", instructions)
     return render_template(
-        "survey_start.html", survey = survey) 
+        "survey_start.html", survey = survey)
 
 @app.post("/begin")
 def begin_survey ():
     """Directs user to question 0 on start click"""
-    responses.clear()
+    session['responses'] = []
     return redirect ("/questions/0")
 
 @app.get("/questions/<int:question_num>")
 def show_question (question_num):
     """Loads a survey question"""
     question = survey.questions[question_num]
-    # prompt = question.prompt
-    # choices = question.choices
+
     return render_template (
-        "question.html", 
+        "question.html",
         question = question,
         question_num = question_num)
 
@@ -42,7 +38,9 @@ def show_question (question_num):
 @app.post('/answer')
 def handle_question_submission ():
     """Appends submission data to responses and redirects to next questioin"""
+    responses = session['responses']
     responses.append(request.form["answer"])
+    session['responses'] = responses
 
     question_num = int(request.form["question_num"]) + 1
     print(responses)
@@ -56,7 +54,8 @@ def handle_question_submission ():
 @app.get('/thank-you')
 def show_thank_you ():
     """Shows a thank you page with list of answers"""
-    
+    responses = session['responses']
+
     return render_template (
         "completion.html",
         questions_length = len(survey.questions),
